4078f70d039830d57534c9379ed44ae1c338b08d
Yalçıncan Ulus <41164799+veotaar@users.noreply.github.com>
2022-10-20 01:48:32 +0300

    Add queue class

---
+++ queue.js
@@ -0,0 +1,17 @@
+export default class Queue {
+  constructor() {
+    this.items = [];
+  }
+
+  enqueue(obj) {
+    this.items.push(obj);
+  }
+
+  dequeue() {
+    return this.items.shift();
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+}
